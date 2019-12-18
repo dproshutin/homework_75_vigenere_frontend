58b67fd6632d93a1bf919725be7b1c415f6e67f3
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-12-18 18:45:21 +0600

    Финальный коммит: удалены комментарии.

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Backdrop.css";
 
 const Backdrop = props => {
-  return props.show ? <div className="Backdrop" onClick={props.clicked} /> : null;
+  return props.show ? <div className="Backdrop" onClick={props.click} /> : null;
 };
 
 export default Backdrop;
